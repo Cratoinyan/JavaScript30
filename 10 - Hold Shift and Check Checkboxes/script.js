3f38917b442bceeb7e302807331c3e36432a3bc5
Umut Ciloglu <umutciloglu06@gmail.com>
2022-01-09 21:09:06 +0300

    add shift select function for top to bottom and get rid of needles shifted function

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,19 +1,24 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
-let shift = false;
 
-checkboxes.forEach(checkboxe => checkboxe.addEventListener('change', checked));
-document.addEventListener('keydown',shifted);
-document.addEventListener('keyup',shifted);
+checkboxes.forEach(checkboxe => checkboxe.addEventListener('click', checked));
 
-function shifted(e){
-    if(e.key == "Shift" && e.type == "keydown")
-        shift = true;
-    else if(e.key == "Shift" && e.type == "keyup")
-        shift = false
-    console.log(shift);
+
+let lastChecked;
+function checked(e){
+    let inBetween = false;
+
+    if(e.shiftKey && this.checked){
+        //console.log(e);
+        checkboxes.forEach(checkbox => {
+            if(checkbox == lastChecked)
+                inBetween = true;
+            else if(inBetween && !checkbox.checked)
+                checkbox.checked = true;
+            else if(checkbox == this)
+                return inBetween = false;
+        })
+    }
+    
+    
+        lastChecked = this;
 }
-
-// function checked(e){
-//     console.log(e);
-//     let lastChecked = this;
-// }
