1723f7fadee69e6565e7cc0bd263060d3556f7a9
Umut Ciloglu <umutciloglu06@gmail.com>
2022-01-15 14:27:00 +0300

    finish project 17 and add it to main menu

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -1,4 +1,14 @@
 
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bandUL = document.querySelector("#bands");
+
+function strip(band){
+    return band.replace(/^(the |a |an )/i, '').trim();
+}
 
 
+const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
+
+bandUL.innerHTML = sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
