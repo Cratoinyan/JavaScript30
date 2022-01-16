4ba488087ecb0a934afb8ece1bfab54ac1a95409
Umut Ciloglu <umutciloglu06@gmail.com>
2022-01-16 14:17:45 +0300

    finish project 21

---
+++ 21 - Geolocation/script.js
@@ -0,0 +1,11 @@
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition((data)=>{
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err) => {
+    console.err(err);
+    console.log('You need to allow the popup for that to work');
+});
